b3de505375aa1b6660c4d947fa02039e3310b738
saket singh <saketsingh@sakets-MacBook-Air.local>
2020-02-22 13:09:14 +0530

    Class component added

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './component/Greet';
+import Welcome from './component/Welcome';
 function App() {
   return (
     <div className="App">
       <Greet/>
+     <Welcome/> 
     </div>
   );
 }
